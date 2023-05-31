cad9baaf42c96f1ed533da3787f63494519a99da
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-05-31 19:09:06 -0300

    Add files via upload
    Se agrega Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import '../../App.css'
+ 
+const Footer = () => {
+    return (
+        <div className='FooterContainer'>
+    <footer>
+      <p className="Copy">&copy; Copyright 2023</p>
+      <p className='Copy'>
+        Desarrollado por 
+        <a href="https://github.com/AlejoVeraCode" target="_blank">
+           Alejo Vera</a>
+      </p>
+    </footer>
+        </div>
+    )
+}
+
+export default Footer
